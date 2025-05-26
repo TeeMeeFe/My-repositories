68606e954115b3ff8843a52ba7a57cdd54e1a356
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2025-05-26 20:59:52 -0300

    Agregar la aplicacion en consola como un menu
    - Tiene 4 modos: crear, editar, mostrar todo, salir
    - Se agrego una funcion para agregar formas(por ahora un rectangulo)
    
    WIP

--- Python/pytests/modulo_2/area_perimetro.py
+++ Python/pytests/modulo_2/area_perimetro.py
@@ -20,6 +20,40 @@ class Rectangulo(Forma):
     def calcular_perimetro(self):
         return 2 * (self.__alto__ + self.__ancho__)
     
-if __name__ == "__main__":
+def agregar_forma(tipo, /, alto, ancho, lista):
+    if(tipo == "Rect"): pass # Sin implementar
+    print(f"Se ha creado un {tipo} de dimensiones alto:{alto} por ancho:{ancho}, con éxito.")
+    return lista.append(Rectangulo(alto, ancho))
     
-    pass
+if __name__ == "__main__":
+    print("Bienvenido al calculador de área y perímetro. Por favor selecciona una de las siguientes opciones:\n")
+    objetos = []
+    while True:
+        sel_opcion = str(input(f"\n[A] Crear un objeto.  \
+                                 \n[E] Editar un objeto. \
+                                 \n[S] Mostrar todos los objetos.  \
+                                 \n[Q] Salir de este programa. \n")).lower()
+        match(sel_opcion):
+            case "a":
+                while True:
+                    try:
+                        print("Vas a crear un nuevo objeto: TIPO RECTANGULO")
+                        #tipo = str(input("")) # Sin implementar
+                        alto = int(input("Alto: "))
+                        ancho = int(input("Ancho: "))
+
+                        agregar_forma("Rect", alto, ancho, objetos)
+                        break
+                    except: 
+                        print("¡El dato que ingresaste no es válido!")
+                        continue
+            case "e":
+                pass
+            case "s":
+                pass
+            case "q":
+                print("Estas saliendo del programa...")           
+                break
+        pass
+
+print("Programa finalizado con éxito.")
