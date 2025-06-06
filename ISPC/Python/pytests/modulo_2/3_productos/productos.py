a939cdd3648d06fcaed982c2f35f8d560200899b
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2025-06-06 20:14:53 -0300

    Agregar 3_productos

---
+++ ISPC/Python/pytests/modulo_2/3_productos/productos.py
@@ -0,0 +1,9 @@
+"""
+3. Producto. Crea una clase Producto con los siguientes atributos: Nombre, Precio
+y Stock siendo obligatorios sólo los atributos Nombre y Precio. El Stock debe
+comenzar con 0. Define métodos para actualizar el stock, para calcular el total
+del inventario y para ver los datos. Además, escribe una aplicación de consola
+que permita al usuario: 1- actualizar el stock y 2- calcular inventario hasta que
+decida detenerse. Al finalizar deberá mostrar los datos del producto, stock e
+inventario final.
+"""
