3ff58711c5341a11bc2db9e078db49ff83f632cf
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2025-06-07 19:55:25 -0300

    Actualizar productos con nuevos metodos clase

--- ISPC/Python/pytests/modulo_2/3_productos/productos.py
+++ ISPC/Python/pytests/modulo_2/3_productos/productos.py
@@ -7,3 +7,86 @@ que permita al usuario: 1- actualizar el stock y 2- calcular inventario hasta qu
 decida detenerse. Al finalizar deberá mostrar los datos del producto, stock e
 inventario final.
 """
+class Producto:
+    _productos = []
+    _id_producto = 0
+
+    def __init__(self, nombre, precio, stock=0):
+        self._nombre = nombre
+        self._precio = precio
+        self._stock = stock
+
+    # Metodos clase
+    @classmethod
+    def mostrar_todos(cls):
+        if(len(cls._productos) != 0):
+            return [
+                {
+                "Índice": u._id_producto,
+                "Nombre": u._nombre,
+                "Precio": u._precio,
+                "Stock" : u._stock
+                }
+                for u in cls._productos
+            ]
+        print("¡El catalogo de productos está desierto!")
+        return None
+
+    @classmethod
+    def obtener_por_id(cls, id_prod):
+        for u in cls._productos:
+            if (u._id_producto == id_prod):
+                return u
+        return None
+    
+    @classmethod
+    def agregar_producto(cls, nom, pre, sto):
+        try:
+            cls._id_producto += 1
+            nuevo_producto = Producto(cls._id_producto, nom, pre, sto)
+            cls._productos.append(nuevo_producto)
+            print(f"El producto {nom} fue agregado con éxito en Índice {cls._id_producto}")
+            return True
+        except Exception as e:
+            print(f"[Error]: {e}")
+            return False
+    
+    @classmethod
+    def actulizar_producto(cls):
+        print("Actualizar stock de productos")
+        if (cls.mostrar_todos() != None and print(cls.mostrar_todos())) : 
+            try:
+                prod = int(input("\n¿Qué producto vas a actualizar? Selecciona su índice: "))
+                if(prod):
+                    while True:
+                        cambio = str(input("\n¿Qué deseas cambiar? Presiona las siguientes teclas:" \
+                                        "\n[S] para actualizar stock" \
+                                        "\n[P] para actualizar precio" \
+                                        "\n[Q] para salir de este programa")).lower()
+                        match(cambio):
+                            case "s":
+                                nuevo_stock = int(input(f"Nuevo stock para '{cls._nombre}': "))
+                                cls._stock = nuevo_stock
+                                print(f"\n¡El stock para '{cls._nombre}' ha sido actualizado con éxito!" /
+                                       "\nNuevo stock: {cls._stock}")
+                                continue
+                            case "p":
+                                nuevo_precio = int(input(f"Nuevo precio para '{cls._nombre}: '"))
+                                cls._precio = nuevo_precio
+                                print(f"¡El precio para {cls._nombre} ha sido cambiado con éxito!" /
+                                       "\nNuevo precio: {cls._precio}")
+                                continue
+                            case "q":
+                                break
+                            case _:
+                                raise ValueError
+                        pass
+            except ValueError:
+                print(f"[Error]: Has ingresado un valor que es inválido, intentalo de nuevo.")
+            except Exception as e:
+                print(f"[Error]: Hubo un error: {e}")
+
+if __name__ == "__main__":
+
+
+    pass
