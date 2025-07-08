e169444a9e56761d7955a79e3b2e484d68f02635
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2025-07-08 18:45:06 -0300

    Improve Formatter by adding another property

--- TOP/js_tests/other_tests/modulePattern.js
+++ TOP/js_tests/other_tests/modulePattern.js
@@ -4,15 +4,24 @@
 const Formatter = (function() {
     console.log("Start");
     const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
+    const timesRun = [];
+    const setTimesRun = () => {
+        log("Setting times run...");
+        timesRun.push(null);
+    };
     const makeUppercase = (text) => {
         log("Making uppercase");
+        setTimesRun();
         return text.toUpperCase();
     };
-
+    
     return {
         makeUppercase,
+        timesRun,
     };
 })();
 
-const hello = Formatter.makeUppercase("Hello");
-console.log(hello);
+console.log(Formatter.makeUppercase("Hello"));
+console.log(Formatter.makeUppercase("Tomek"));
+console.log(Formatter.makeUppercase("linus"));
+console.log(Formatter.timesRun.length);
