857a21694dc882c8940b556dea0e46fc55509cbc
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2025-09-06 17:15:27 -0300

    Agregar modulo errorcode para el manejo de errores en la conexion a la BD

--- ISPC/Python/pytests/tests_desafios/connect_db.py
+++ ISPC/Python/pytests/tests_desafios/connect_db.py
@@ -1,20 +1,34 @@
 import mysql.connector
+from mysql.connector import errorcode
 
 # Conectamos a una base de datos MySQL local
-conn = mysql.connector.connect(
-    user = "root",
-    password = "",
-    host = "localhost",
-    database = "test",
-    port = "3307"
-)
+def conectarABD():
+    try:
+        return mysql.connector.connect(
+            user = "root",
+            password = "",
+            host = "localhost",
+            database = "test", 
+            port = "3307"
+        )
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("El usuario o contraseña no son correctos")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR | errorcode.ER_NO_DB_ERROR:
+            print("Error en la conexión a la base de datos")
+        else:
+            print(err)
+    return None
+
+conn = conectarABD()
+if not isinstance(conn, object): exit() # Salimos del programa si algo salio mal
 
 # Creamos un cursor para ejecutar sentencias SQL
 cursor = conn.cursor()
 
 # Creamos una tabla si no existe
 e = cursor.execute("""
-    CREATE TABLE IF NOT EXISTS usuarios (s
+    CREATE TABLE IF NOT EXISTS usuarios (
         id INTEGER PRIMARY KEY,
         nombre TEXT,
         edad INTEGER
@@ -22,8 +36,8 @@ e = cursor.execute("""
 """)
 
 # Insertamos datos en la tabla
-cursor.execute("INSERT INTO usuarios (id, nombre, edad) VALUES (%s, %s, %s)", (1, "ana", 30))
-cursor.execute("INSERT INTO usuarios (id, nombre, edad) VALUES (%s, %s, %s)", (2, "bob", 25))
+#cursor.execute("INSERT INTO usuarios (id, nombre, edad) VALUES (%s, %s, %s)", (1, "ana", 30))
+#cursor.execute("INSERT INTO usuarios (id, nombre, edad) VALUES (%s, %s, %s)", (2, "bob", 25))
 conn.commit()
 
 # Realizamos una consulta SQL
