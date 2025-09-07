2c8cbddc8c1187ba5453267181c8b01f0144a1e1
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com.>
2025-09-06 21:15:51 -0300

    Ahora insert.py puede insertar multiples usuarios

--- ISPC/Python/pytests/tests_desafios/connect_db/insert.py
+++ ISPC/Python/pytests/tests_desafios/connect_db/insert.py
@@ -3,19 +3,22 @@ import mysql.connector
 
 conn = connectToDB()
 
-# Parametros para usuario nuevo
-nombre = "Maira"        
-edad = 18
+# Parametros para varios usuarios nuevos
+nuevos_usuarios = [
+    ("Camila", 19),
+    ("Tobias", 25),
+    ("Verónica", 31)
+]
 
 if conn.is_connected:
     try:
         with conn.cursor() as cursor:
             # Generamos la insercion de una fila con los datos en la tabla
             query = "INSERT INTO usuarios (nombre, edad) VALUES (%s, %s)"
-            cursor.execute(query, (nombre, edad,))
+            cursor.executemany(query, nuevos_usuarios)
             logger.info(cursor)
             conn.commit()
-            logger.info("Usuario registrado con exito.")
+            logger.info(f"{cursor.rowcount} usuario/s registrado/s con exito.")
     except mysql.connector.Error as err:
         logger.error(err)
         raise f"Error al insertar, {err}"
